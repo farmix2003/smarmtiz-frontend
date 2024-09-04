eb4c09165b5a8dec34f32012cbac6bdbeb33621e
Farmix2003 <ffarrux386@gmail.com>
2024-09-04 13:05:21 +0300

    success message

--- src/app/panel/page.jsx
+++ src/app/panel/page.jsx
@@ -5,11 +5,12 @@ import React, { useState } from "react";
 
 const AdminPanel = () => {
   const [courseName, setCourseName] = useState("");
-  const [coursePrice, setCoursePrice] = useState(0);
+  const [coursePrice, setCoursePrice] = useState();
   const [courseType, setCourseType] = useState("");
   const [courseTime, setCourseTime] = useState("");
   const [imageLink, setImageLink] = useState("");
-
+  const [msg, setMsg] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
   const handleAddNewCourse = async (e) => {
     e.preventDefault();
     try {
@@ -20,12 +21,18 @@ const AdminPanel = () => {
         courseType,
         imageLink
       );
+      setIsSuccess(true);
+      setMsg("Kurs muvvafaqiyatli qo'shildi");
+      setTimeout(() => {
+        setIsSuccess(false);
+        setMsg("");
+      }, 3000);
+      console.log("Course added successfully:", res);
       setCourseName("");
       setCoursePrice(0);
       setCourseType("");
       setCourseTime("");
       setImageLink("");
-      console.log("Course added successfully:", res);
     } catch (error) {
       console.error("Error adding course:", error.message);
       // Optionally display the error to the user
@@ -33,43 +40,52 @@ const AdminPanel = () => {
   };
 
   return (
-    <form
-      onSubmit={handleAddNewCourse}
-      className="bg-gray-500/40 rounded-md p-4 w-[250px] flex flex-col justify-center mt-10 mx-auto"
-    >
-      <InputForm
-        type={"text"}
-        name={"Kurs nomi: "}
-        value={courseName}
-        change={setCourseName}
-      />
-      <InputForm
-        type={"number"}
-        name={"Kurs narxi: "}
-        value={coursePrice}
-        change={setCoursePrice}
-      />
-      <InputForm
-        type={"text"}
-        name={"Kurs turi: "}
-        value={courseType}
-        change={setCourseType}
-      />
-      <InputForm
-        type={"text"}
-        name={"Kurs soati: "}
-        placeHolder={"Misol 1.5 soat"}
-        value={courseTime}
-        change={setCourseTime}
-      />
-      <InputForm
-        type={"text"}
-        name={"Rasm linki: "}
-        value={imageLink}
-        change={setImageLink}
-      />
-      <button className="mt-3 p-2 bg-slate-600/30 rounded-md">Qo'shish</button>
-    </form>
+    <div className="w-screen">
+      {isSuccess && (
+        <div className="bg-green-500 absolute ml-[80%] text-white p-2 rounded-md text-center">
+          {msg}
+        </div>
+      )}
+      <form
+        onSubmit={handleAddNewCourse}
+        className="bg-gray-500/40 rounded-md p-4 w-[250px] flex flex-col justify-center mt-10 mx-auto"
+      >
+        <InputForm
+          type={"text"}
+          name={"Kurs nomi: "}
+          value={courseName}
+          change={setCourseName}
+        />
+        <InputForm
+          type={"number"}
+          name={"Kurs narxi: "}
+          value={coursePrice}
+          change={setCoursePrice}
+        />
+        <InputForm
+          type={"text"}
+          name={"Kurs turi: "}
+          value={courseType}
+          change={setCourseType}
+        />
+        <InputForm
+          type={"text"}
+          name={"Kurs soati: "}
+          placeHolder={"Misol 1.5 soat"}
+          value={courseTime}
+          change={setCourseTime}
+        />
+        <InputForm
+          type={"text"}
+          name={"Rasm linki: "}
+          value={imageLink}
+          change={setImageLink}
+        />
+        <button className="mt-3 p-2 bg-slate-600/30 rounded-md">
+          Qo'shish
+        </button>
+      </form>
+    </div>
   );
 };
 
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -3,14 +3,15 @@ import React from "react";
 const InputForm = ({ type, name, placeHolder, value, change }) => {
   return (
     <div>
-      <label htmlFor="">{name}</label>
+      <label htmlFor={name}>{name}</label>
       <input
         className="border-none outline-none text-black"
         type={type}
         id="courseName"
-        name="courseName"
+        name={name}
         placeholder={placeHolder}
         value={value}
+        required
         onChange={(e) => change(e.target.value)}
       />
     </div>
