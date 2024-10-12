f5d7a35a9b5f1a9aa8b59610697322a7bf4e2854
Farrukh <119849180+farmix2003@users.noreply.github.com>
2024-10-12 16:03:14 +0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://smartwebsite-2dd5c535c591.herokuapp.com',
+    baseURL: 'https://smartmiz-2462bfd28095.herokuapp.com',
     // baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json'
