5f44421152b1f868134f5ec2fbc9084dbcbabac1
Farrukh <119849180+farmix2003@users.noreply.github.com>
2025-01-24 09:10:43 +0200

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://smartmiz-2462bfd28095.herokuapp.com',
+    baseURL: 'https://smartmizbackend.onrender.com',
     // baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json'
